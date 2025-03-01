2471a419d6a82e6d90f7dd606ec3e8b8ff6dbe96
Yaasir Aidil Fitrah <yaasiraidilfitrah@gmail.com>
2025-03-02 01:59:08 +0700

    Menambahkan Fungsi alert jika data pasien tidak ada

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
 import { Form, Col, Button, InputGroup, Row, Container } from "react-bootstrap";
-import data from "../../datapasien.json"; 
+import Swal from "sweetalert2"; // Import SweetAlert2
+import data from "../../datapasien.json";
 
 const SearchBar = ({ onSelectPatient }) => {
   const [nikNrm, setNikNrm] = useState("");
-
 
   const handleSearch = () => {
     const found = data.find(
       (item) => item.NIK === nikNrm || item.NomorRekamMedis === nikNrm
     );
-    if (onSelectPatient && typeof onSelectPatient === "function") {
-      onSelectPatient(found || null);
+
+    if (found) {
+      if (onSelectPatient && typeof onSelectPatient === "function") {
+        onSelectPatient(found);
+      } else {
+        console.error("onSelectPatient is not a function");
+      }
     } else {
-      console.error("onSelectPatient is not a function");
+      // Tampilkan alert jika data tidak ditemukan
+      Swal.fire({
+        icon: "error",
+        title: "Data Tidak Ditemukan",
+        text: "Data pasien dengan NIK/Nomor Rekam Medis tersebut tidak ditemukan.",
+        confirmButtonText: "OK",
+      });
     }
   };
-
 
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
